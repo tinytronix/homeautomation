a34f1b5654e5d1e561f6acffffd72fbb1639ea43
tinytronix <49196001+tinytronix@users.noreply.github.com>
2023-01-21 17:00:07 +0100

    Update tftpflash.sh

--- Software/Controllermodul_D/tftpflash.sh
+++ Software/Controllermodul_D/tftpflash.sh
@@ -4,7 +4,8 @@ ARDUINODIRECTORY=/set/your/arduino/directory/here
 PROJECTNAME=nameOfYourProject
 
 #jump to bootloader 
-#Attention: This sequence is application specific. You need to implement it in your application.
+#Attention: This sequence is application specific. You need to implement it in your application if you want the flash procedure to start automatically.
+#Otherwise you have to reboot manually before starting this script
 echo -e "\xAA\x55\xAA\x55" | nc -w 0 -4u 192.168.1.128 46001
 
 cd $ARDUINODIRECTORY
