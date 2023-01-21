e64505f389c785791752ba0a76c79cec4e171e6d
tinytronix <49196001+tinytronix@users.noreply.github.com>
2023-01-21 16:58:37 +0100

    Update tftpflash.sh

--- Software/Controllermodul_D/tftpflash.sh
+++ Software/Controllermodul_D/tftpflash.sh
@@ -8,7 +8,7 @@ PROJECTNAME=nameOfYourProject
 echo -e "\xAA\x55\xAA\x55" | nc -w 0 -4u 192.168.1.128 46001
 
 cd $ARDUINODIRECTORY
-avr-objcopy -I ihex $ARDUINODIRECTORY/$PROJECTNAME.ino.arduino_standard.hex -O binary $ARDUINODIRECTORY/$PROJECTNAME/x.bin
+avr-objcopy -I ihex $ARDUINODIRECTORY/$PROJECTNAME.ino.arduino_standard.hex -O binary $ARDUINODIRECTORY/x.bin
 sleep 0.6
 tftp 192.168.1.128 <<'EOF'
 mode octet
